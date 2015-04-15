43d1648e79496bf5b48da250af3f37257eab87e9
Andrew Chalmers <andrewchalmers26@gmail.com>
2015-04-15 12:56:42 +1200

    Updated comments

--- EMD.py
+++ EMD.py
@@ -6,6 +6,8 @@ Created on 11/04/2015
 This code computes the Earth Mover's Distance, as explained here:
 http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/RUBNER/emd.htm
 
+This is done using numpy, scipy (minimize)
+
 There is a simple example of two distributions computed by getExampleSignatures()
 This example is chosen in order to compare the result with a C implementation 
 found here:
